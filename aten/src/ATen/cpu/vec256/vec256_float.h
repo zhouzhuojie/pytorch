3fa1eafb214aba6dd3a71974990f118e57d7f6b7
Hong Xu <hong@topbug.net>
2021-04-26 23:17:58 -0700

    Vectorize fmax and fmin for float and double on CPU
    ghstack-source-id: fa4ef5cf5d76f5ca75883e2f71d95ee87e476e9a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/55867

--- aten/src/ATen/cpu/vec256/vec256_float.h
+++ aten/src/ATen/cpu/vec256/vec256_float.h
@@ -345,6 +345,30 @@ Vec256<float> inline minimum(const Vec256<float>& a, const Vec256<float>& b) {
   return _mm256_or_ps(min, isnan);
 }
 
+// std::fmax, which returns the non-NaN input if either input is a NaN.
+template <>
+inline Vec256<float> fmax(const Vec256<float>& a, const Vec256<float>& b) {
+  // Make sure all NaN's are filled with bit 1, including the sign bit
+  Vec256<float> a_normalized = _mm256_or_ps(_mm256_cmp_ps(a, a, _CMP_UNORD_Q), a);
+  Vec256<float> b_normalized = _mm256_or_ps(_mm256_cmp_ps(b, b, _CMP_UNORD_Q), b);
+  // Exploit the fact that all-ones is a NaN, and the second input is returned if either input is NaN.
+  Vec256<float> max_ab = _mm256_max_ps(a_normalized, b_normalized);
+  Vec256<float> max_ba = _mm256_max_ps(b_normalized, a_normalized);
+  return _mm256_and_ps(max_ab, max_ba);
+}
+
+// std::fmin, which returns the non-NaN input if either input is a NaN.
+template <>
+inline Vec256<float> fmin(const Vec256<float>& a, const Vec256<float>& b) {
+  // Make sure all NaN's are filled with bit 1, including the sign bit
+  Vec256<float> a_normalized = _mm256_or_ps(_mm256_cmp_ps(a, a, _CMP_UNORD_Q), a);
+  Vec256<float> b_normalized = _mm256_or_ps(_mm256_cmp_ps(b, b, _CMP_UNORD_Q), b);
+  // Exploit the fact that all-ones is a NaN, and the second input is returned if either input is NaN.
+  Vec256<float> min_ab = _mm256_min_ps(a_normalized, b_normalized);
+  Vec256<float> min_ba = _mm256_min_ps(b_normalized, a_normalized);
+  return _mm256_and_ps(min_ab, min_ba);
+}
+
 template <>
 Vec256<float> inline clamp(const Vec256<float>& a, const Vec256<float>& min, const Vec256<float>& max) {
   return _mm256_min_ps(max, _mm256_max_ps(min, a));
